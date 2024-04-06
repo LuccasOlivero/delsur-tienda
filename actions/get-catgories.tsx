dc9d461b39e1b48115ebcb98f3ac7a69018e3eca
lucas <lucas.chorolqui@gmail.com>
2024-04-06 10:09:59 -0300

    pages, fetch to db, and styles added

---
+++ actions/get-catgories.tsx
@@ -0,0 +1,20 @@
+import { Category } from "@/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
+
+let randomNumberGenerator = (() => Math.ceil(Math.random() * 100000))();
+setInterval(() => (randomNumberGenerator = Math.ceil(Math.random() * 60)), 30); // llamamos a la funtion randomNumberGenerator() cada 30s. Con esto el numero en max-age=X cambia cada 30s, cuando cambia el numero de max-age y se refresca la pagina se hace nuevamente la peticion a al servidor
+
+export default async function getCategories(): Promise<Category[]> {
+  const res = await fetch(URL, {
+    headers: {
+      // "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
+      "Cache-Control": `must-revalidate, max-age=${randomNumberGenerator}`,
+    },
+  });
+  const data = await res.json();
+  return data;
+}
+
+// must-revalidate: Indica que el navegador o proxy debe revalidar la respuesta con el servidor antes de utilizarla, incluso si está en caché. Si no puede validarla, debe volver a solicitarla al servidor.
+// max-age: Especifica el tiempo máximo en segundos que un recurso puede ser almacenado en caché.
---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10 ">
+        <p className="text-center text-xs text-black">
+          &copy; todos los derechos resrvaods 2000
+        </p>
+      </div>
+    </div>
+  );
+}
