96a1a50c0e7857c983c544e05b56951a35dac31f
lucas <lucas.chorolqui@gmail.com>
2024-06-05 15:45:26 -0300

    update - better calls at actions

--- actions/get-all-products.ts
+++ actions/get-all-products.ts
@@ -2,19 +2,12 @@ import { Product } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-let randomNumberGenerator = (() => Math.ceil(Math.random() * 100000))();
-setInterval(() => (randomNumberGenerator = Math.ceil(Math.random() * 60)), 30); // llamamos a la funtion randomNumberGenerator() cada 30s. Con esto el numero en max-age=X cambia cada 30s, cuando cambia el numero de max-age y se refresca la pagina se hace nuevamente la peticion a al servidor
-
 export default async function getAllProducts(): Promise<Product[]> {
   const res = await fetch(URL, {
     headers: {
-      // "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
-      "Cache-Control": `must-revalidate, max-age=${randomNumberGenerator}`,
+      "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
     },
   });
   const data = await res.json();
   return data;
 }
-
-// must-revalidate: Indica que el navegador o proxy debe revalidar la respuesta con el servidor antes de utilizarla, incluso si está en caché. Si no puede validarla, debe volver a solicitarla al servidor.
-// max-age: Especifica el tiempo máximo en segundos que un recurso puede ser almacenado en caché.
--- actions/get-products.ts
+++ actions/get-products.ts
@@ -9,9 +9,6 @@ interface Query {
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-let randomNumberGenerator = (() => Math.ceil(Math.random() * 100000))();
-setInterval(() => (randomNumberGenerator = Math.ceil(Math.random() * 60)), 30); // llamamos a la funtion randomNumberGenerator() cada 30s. Con esto el numero en max-age=X cambia cada 30s, cuando cambia el numero de max-age y se refresca la pagina se hace nuevamente la peticion a al servidor
-
 export default async function getProductsFeatured(
   query: Query
 ): Promise<Product[]> {
@@ -26,13 +23,9 @@ export default async function getProductsFeatured(
 
   const res = await fetch(url, {
     headers: {
-      // "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
-      "Cache-Control": `must-revalidate, max-age=${randomNumberGenerator}`,
+      "Cache-Control": "must-revalidate, no-cache, no-store, max-age=5",
     },
   });
   const data = await res.json();
   return data;
 }
-
-// must-revalidate: Indica que el navegador o proxy debe revalidar la respuesta con el servidor antes de utilizarla, incluso si está en caché. Si no puede validarla, debe volver a solicitarla al servidor.
-// max-age: Especifica el tiempo máximo en segundos que un recurso puede ser almacenado en caché.
